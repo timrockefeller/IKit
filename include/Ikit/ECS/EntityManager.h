6d7bd0c738ae82351b8c310779bfcdefd5232c57
kitekii <tim720719@163.com>
2020-10-20 23:16:10 +0800

    add essential

---
+++ include/Ikit/ECS/EntityManager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+
+namespace KTKR::ECS
+{
+    class EntityManager
+    {
+    private:
+    
+    public:
+        EntityManager()= default;
+    };
+    
+} // namespace KTKR
---
+++ include/Ikit/ECS/World.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace KTKR::ECS {
+class EntityManager;
+class World {
+   public:
+    EntityManager entityManager;
+
+   private:
+   public:
+    World()=default;
+};
+
+}  // namespace KTKR::ECS

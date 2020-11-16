207f74a49a3466bede7f6a015a5de96472064eb8
kitekii <tim720719@163.com>
2020-11-16 16:27:15 +0800

    Achetype init

---
+++ include/Ikit/ECS/details/Archetype.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Ikit/Pooling/Pool.h>
+#include <vector>
+#include "Chunk.h"
+
+namespace KTKR::ECS {
+class Archetype {
+   public:
+    Archetype(/* args */);
+
+
+    // disable copying
+    Archetype(const Archetype&) = delete;
+    Archetype& operator=(const Archetype&) = delete;
+    ~Archetype();
+
+    void At();
+    Chunk* GetChunk(size_t idx) const { return idx<chunks.size()> idx ? chunks[idx] : nullptr; }
+
+   private:
+    size_t chunkCapacity{static_cast<size_t>(-1)};
+    Pool<Chunk> chunkPool;
+    std::vector<Chunk*> chunks;
+};
+
+}  // namespace KTKR::ECS

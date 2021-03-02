49a123fc6bcb5aeed90f5a33d94d4ed2160e8733
kitekii <tim720719@163.com>
2021-03-02 23:21:13 +0800

    Exporter

--- cmake/IKPackage.cmake
+++ cmake/IKPackage.cmake
@@ -56,3 +56,59 @@ endfunction()
 function(IK_SetupTarget)
 
 endfunction()
+
+function(IK_AddPackage _name version)
+# 添加第三方依赖包
+include(FetchContent)
+# FetchContent_MakeAvailable was not added until CMake 3.14
+if(${CMAKE_VERSION} VERSION_LESS 3.14)
+    include(add_FetchContent_MakeAvailable.cmake)
+endif()
+message(STATUS "importing ${_name}")
+set(IKIT_GIT_TAG  ${version})  # 指定版本
+set(IKIT_GIT_URL  "https://github.com/timrockefeller/${_name}.git")  # 指定git仓库地址
+FetchContent_Declare(
+  ${_name}
+  GIT_REPOSITORY    ${IKIT_GIT_URL}
+  GIT_TAG           ${IKIT_GIT_TAG}
+)
+FetchContent_MakeAvailable(${_name})
+endfunction()
+
+macro (IK_Export)
+  cmake_parse_arguments("ARG" "TARGET" "" "DIRECTORIES" ${ARGN})
+  message(STATUS "- export ${PROJECT_NAME} ${PROJECT_VERSION}")
+  
+  include(CMakePackageConfigHelpers)
+  # generate the config file that is includes the exports
+  configure_package_config_file(${PROJECT_SOURCE_DIR}/config/Config.cmake.in
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    INSTALL_DESTINATION "${package_name}/cmake"
+    NO_SET_AND_CHECK_MACRO
+    NO_CHECK_REQUIRED_COMPONENTS_MACRO
+  )
+  
+  # generate the version file for the config file
+  write_basic_package_version_file(
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY SameMinorVersion
+  )
+
+  # install the configuration file
+  install(FILES
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    DESTINATION "${package_name}/cmake"
+  )
+  
+  foreach(dir ${ARG_DIRECTORIES})
+    string(REGEX MATCH "(.*)/" prefix ${dir})
+    if("${CMAKE_MATCH_1}" STREQUAL "")
+      set(_destination "${package_name}")
+    else()
+      set(_destination "${package_name}/${CMAKE_MATCH_1}")
+    endif()
+    install(DIRECTORY ${dir} DESTINATION "${_destination}")
+  endforeach()
+  endmacro()

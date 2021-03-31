5d1e0e93f3e2409982073bb2da24462c25546f73
kitekii <tim720719@163.com>
2021-03-31 08:37:02 +0800

    fix singleton permission in mutex

--- include/Ikit/STL/Singleton.h
+++ include/Ikit/STL/Singleton.h
@@ -1,32 +1,22 @@
 #pragma once
-#include <mutex>
+namespace KTKR
+{
+    template <typename T>
+    class Singleton
+    {
+    public:
+        Singleton(const Singleton &) = delete;
+        Singleton(Singleton &&) = delete;
 
-namespace KTKR {
-template <typename T>
-class Singleton {
-   private:
-    /* data */
-   public:
-    Singleton(const Singleton&) = delete;
-    Singleton(Singleton&&) = delete;
-    virtual ~Singleton() = default;
+        const Singleton &operator=(const Singleton &) = delete;
+        const Singleton &operator=(Singleton &&) = delete;
+        static T* Get()
+        {
+            static T* instance = new T;
+            return instance;
+        }
+        virtual ~Singleton() = default;
+        Singleton() = default;
+    };
 
-    const Singleton& operator=(const Singleton&) = delete;
-    const Singleton& operator=(Singleton&&) = delete;
-    static T& Get() {
-        static std::once_flag flag;
-        std::call_once(flag, &Singleton::GetInstance);
-        return GetInstance();
-    }
-
-   protected:
-    Singleton() {}
-
-   private:
-    static T& GetInstance() {
-        static T instance;
-        return instance;
-    }
-};
-
-}  // namespace KTKR
+} // namespace KTKR
